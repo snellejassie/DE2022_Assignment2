5131bb7ca687eb4f3a0ff6f8452650de13c01666
snellejassie <99133513+snellejassie@users.noreply.github.com>
2022-11-28 15:52:43 +0100

    Update IP

--- kafka-producer/kafkaadmin.py
+++ kafka-producer/kafkaadmin.py
@@ -10,7 +10,7 @@ def create_topics(admin, topic_list):
 
 
 if __name__ == '__main__':
-    admin_client = KafkaAdminClient(bootstrap_servers="VM_IP:9092",
+    admin_client = KafkaAdminClient(bootstrap_servers="35.187.34.200:9092",
                                     client_id='Lab8')  # use your VM's external IP Here!
     topic_list = [NewTopic(name="word", num_partitions=1, replication_factor=1),
                   NewTopic(name="wordcount", num_partitions=1, replication_factor=1)]
